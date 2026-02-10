3a8920793ec8475b81976921e48f5375f3565f83
Rik Meijer <nostriphant@rikmeijer.nl>
2026-02-10 11:47:11 +0100

    wrap When in Alternate

--- src/Alternate.php
+++ src/Alternate.php
@@ -5,22 +5,22 @@ namespace nostriphant\Functional;
 readonly class Alternate {
 
     private function __construct(
-            public \Closure $callback
+            public When $when
     ) {
         
     }
 
-    static function __callStatic(string $name, array $arguments): When {
-        return new When(
+    static function __callStatic(string $name, array $arguments): self {
+        return new self(new When(
             fn(callable ...$callbacks) => array_key_exists($name, $callbacks),
             function(callable ...$callbacks) use ($name, $arguments) { 
                 yield from $callbacks[$name](...$arguments);
             }, 
             $name !== 'default' ? self::default(...$arguments) : function() { yield from []; }
-        );
+        ));
     }
 
-    public function __invoke(callable ...$callbacks) {
-        yield from ($this->callback)(...$callbacks);
+    public function __invoke(callable ...$callbacks) : \Generator {
+        yield from ($this->when)(...$callbacks);
     }
 }

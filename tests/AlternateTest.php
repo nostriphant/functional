0b4df86b5380cfcf1bd3958f0cbb0429afeee264
Rik Meijer <nostriphant@rikmeijer.nl>
2026-02-10 12:11:01 +0100

    Revert "refactor"
    This reverts commit 91b8226d7b73e80a17fe677b06d85e6f790c46bd.

--- tests/AlternateTest.php
+++ tests/AlternateTest.php
@@ -4,33 +4,7 @@ namespace nostriphant\FunctionalTests;
 
 use nostriphant\Functional\Alternate;
 
-expect()->extend('toHaveState', function (mixed ...$expected_states) {
-    $state_mocks = array_map(fn(mixed $expected_args) => new class($expected_args) {
-
-                public bool $invoked = false;
-
-                public function __construct(private array|string $expected_args) {
-
-                }
-
-                public function __invoke(mixed ...$args): \Generator {
-                    if (is_array($this->expected_args)) {
-                        foreach ($this->expected_args as $index => $expected_arg) {
-                            expect($args[$index])->toBe($expected_arg);
-                        }
-                    } elseif ($this->expected_args === '*') {
-                        // accept everything!
-                    }
-                    $this->invoked = true;
-                    yield 0;
-                }
-            }, $expected_states);
-
-    foreach (call_user_func_array($this->value, $state_mocks) as $msg) {
-
-    }
-    expect(array_reduce($state_mocks, fn(bool $carry, $mock) => $mock->invoked || $carry, false))->toBeTrue();
-});
+Pest::extend('expect');
 
 it('usage works', function () {
     $state = Alternate::state1('Hello World!');

fd10cef62bbc414ca551a3ced0110ce530659a22
Rik Meijer <nostriphant@rikmeijer.nl>
2026-06-08 13:22:54 +0200

    type-hinting safe Partial application

--- src/Partial.php
+++ src/Partial.php
@@ -4,9 +4,28 @@ namespace nostriphant\Functional;
 
 class Partial {
     static function left(callable $f, mixed ...$partial_args) {
-        return fn(mixed ...$args) => call_user_func($f, ...$partial_args, ...$args);
+        return match($f instanceof \Closure) {
+            true => fn(mixed ...$args) => call_user_func($f, ...$partial_args, ...$args),
+            false => new (self::wrap_code(get_class($f), '...$this->partial_args, ...$args'))($f, $partial_args)
+        };
     }
     static function right(callable $f, mixed ...$partial_args) {
-        return fn(mixed ...$args) => call_user_func($f, ...$args, ...$partial_args);
+        return match($f instanceof \Closure) {
+            true => fn(mixed ...$args) => call_user_func($f, ...$args, ...$partial_args),
+            false => new (self::wrap_code(get_class($f), '...$args, ...$this->partial_args'))($f, $partial_args)
+        };
+    }
+    
+    static function wrap_code(string $wrapped_class, string $apply_code) : string {
+            $wrapper_class = '_'.uniqid();
+        eval('class ' . $wrapper_class . ' extends '. $wrapped_class .' {
+                function __construct(private $f, private $partial_args) {
+                
+                }
+                public function __invoke(...$args): mixed {
+                    return call_user_func($this->f, '.$apply_code.');
+                }
+            }');
+        return $wrapper_class;
     }
 }
--- tests/PartialTest.php
+++ tests/PartialTest.php
@@ -16,3 +16,24 @@ it('partially applies arguments right', function () {
     $f_applied = \nostriphant\Functional\Partial::right($f, 1);
     expect($f_applied(4))->toBe(3);
 });
+
+class Substract {
+    public function __invoke(int $a, int $b): mixed {
+        return $a - $b;
+    }
+}
+
+it('partially applies arguments left (type hinting safe)', function () {
+    $f = new Substract();
+    
+    $f_applied = \nostriphant\Functional\Partial::left($f, 1);
+    expect($f_applied(2))->toBe(-1);
+    expect($f_applied)->toBeInstanceOf(Substract::class);
+});
+it('partially applies arguments right (type hinting safe)', function () {
+    $f = new Substract();
+    
+    $f_applied = \nostriphant\Functional\Partial::right($f, 1);
+    expect($f_applied(4))->toBe(3);
+    expect($f_applied)->toBeInstanceOf(Substract::class);
+});
